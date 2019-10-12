020f3e75d9a91c1f02b93ad465bcd54e5cc2cdae
tianxiangbing <55342775@qq.com>
2019-10-12 10:06:30 +0800

    'x'

--- jest.setup.js
+++ jest.setup.js
@@ -2,4 +2,25 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+/**
+ * Set up DOM in node.js environment for Enzyme to mount to
+ */
+const { JSDOM } = require('jsdom');
+
+const jsdom = new JSDOM('<!doctype html><html><body>123</body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+copyProps(window, global);
 configure({ adapter: new Adapter() });
--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,10 +1,40 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Mask from '../components/Mask';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { show: false };
+    }
+    testFunc() {
+        let m = Mask.show();
+        setTimeout(() => {
+            Mask.hide()
+        }, 2000)
+    }
+    testFunc2() {
+        // Mask.show({ container: document.getElementById('container'), mask: true })
+        ReactDOM.createPortal(<div className="x-mask">222</div>, document.body)
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.testFunc.bind(this)}>测试方法</button>
+                <button onClick={this.testFunc2.bind(this)}>指定容器loading</button>
+                <div className="container" style={{ backgroundColor: '#ececec', height: '500px' }} id="container"></div>
+            </div>
+        )
+    }
+}
+
+const Test = mount(<App />)
 
 describe('Test Mask', () => {
     it('测试初始化显示', () => {
-        let m = Mask.show();
-        expect(document.body.querySelector('.x-mask')).not.toBeNull()
+        console.log(Test)
+        Test.instance().testFunc2();
+        expect(document.body.querySelector('.x-mask')).toBeNull()
     });
 });
